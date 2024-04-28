011f21cf9d5c5d034a792b8e564433f670fc5dc2
Dihep <dihep.demayo@gmail.com>
2024-04-28 19:55:58 +0200

    "Dadi" completato.

--- js/dadi.js
+++ js/dadi.js
@@ -1,3 +1,15 @@
 // Genera due numeri random tra 1 e 6 (inclusi), uno per il giocatore e uno per il computer.
-console.log("JS is here.");
+let player = Math.floor(Math.random()*7);
+console.log("Player rolled: ", player);
+let computer = Math.floor(Math.random()*7);
+console.log("Computer rolled: ", computer);
+
 //     Stabilisci il vincitore, in base a chi ha ottenuto il punteggio più alto. 
+let result = "IT'S A DRAW!";
+if (player>computer) {
+    result = "YOU WIN!";
+}
+else if (player<computer) {
+    result = "YOU LOSE!";
+};
+console.log("Epic dice match result: ", result);
