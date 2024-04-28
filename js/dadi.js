67940b0509508bee25e76e0a74cc4f5c6ad16890
Dihep <dihep.demayo@gmail.com>
2024-04-28 19:48:25 +0200

    first commit

---
+++ js/dadi.js
@@ -0,0 +1,3 @@
+// Genera due numeri random tra 1 e 6 (inclusi), uno per il giocatore e uno per il computer.
+console.log("JS is here.");
+//     Stabilisci il vincitore, in base a chi ha ottenuto il punteggio più alto. 
